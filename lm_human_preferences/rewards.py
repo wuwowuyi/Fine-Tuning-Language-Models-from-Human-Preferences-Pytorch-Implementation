b92174ef1dd1f7241ad8e1b0c4c56710152ff5e2
wuwowuyi <dalucheng@gmail.com>
2024-02-09 21:25:08 -0800

    comment out model for policy for now

--- lm_human_preferences/rewards.py
+++ lm_human_preferences/rewards.py
@@ -1,11 +1,7 @@
 """Synthetic scores."""
 
-import os
-
 import torch
 from torch import nn
-
-from lm_human_preferences.language import trained_models, model
 
 
 # TODO: sort out device and gradient!
@@ -61,73 +57,73 @@ class RewardModel(nn.Module):
         )
 
 
-class TrainedRewardModel():
-    def __init__(self, train_dir, encoding, *, scope='reward_model', comm=MPI.COMM_WORLD):
-        self.train_dir = train_dir
-        self.comm = comm
-
-        self.encoding = encoding
-        encoder = encoding.get_encoder()
-        if train_dir != 'test':
-            self.hparams = trained_models.load_hparams(os.path.join(train_dir, 'hparams.json'))
-            assert self.hparams.n_vocab == encoding.n_vocab, f'{self.hparams.n_vocab} != {encoding.n_vocab}'
-        else:
-            self.hparams = trained_models.test_hparams()
-
-        self.padding_token = encoder.padding_token
-
-        self.encoder = encoder
-
-        self.scope = scope
-        self.model = model.Model(hparams=self.hparams, scope=f'{scope}/model', scalar_heads=['reward'])
-
-    def _build(self, X):
-        results = self.model(X=X, padding_token=self.padding_token)
-        reward = results['reward'][:, -1]
-        with tf.compat.v1.variable_scope(f'{self.scope}/reward_norm'):
-            self.reward_gain = tf.compat.v1.get_variable('gain', shape=(), initializer=tf.compat.v1.constant_initializer(1))
-            self.reward_bias = tf.compat.v1.get_variable('bias', shape=(), initializer=tf.compat.v1.constant_initializer(0))
-        reward = self.reward_gain * reward + self.reward_bias
-        self._set_initializers()
-        return reward
-
-    def ensure_built(self):
-        if self.model.built:
-            return
-        with tf.compat.v1.name_scope('dummy'):
-            self._build(X=tf.zeros([0,0], dtype=tf.int32))
-
-    def _set_initializers(self):
-        """Change initializers to load a model from a tensorflow checkpoint."""
-        if self.comm.Get_rank() > 0 or self.train_dir == 'test':
-            return
-
-        assert self.model.built
-        checkpoint_scope = 'reward_model'
-
-        with tf.init_scope():
-            # Initialize!
-            params = {v.op.name: v for v in self.get_params()}
-            checkpoint = tf.train.latest_checkpoint(os.path.join(self.train_dir, 'checkpoints/'))
-            available = tf.train.list_variables(checkpoint)
-            unchanged = {}
-
-            for name, shape in available:
-                if not name.startswith(checkpoint_scope + '/'):
-                    # print('skipping', name)
-                    continue
-                if name.endswith('adam') or name.endswith('adam_1'):
-                    # print('skipping', name)
-                    continue
-                print('setting', name)
-                var = params[self.scope + name[len(checkpoint_scope):]]
-                assert var.shape == shape, 'Shape mismatch: %s.shape = %s != %s' % (var.op.name, var.shape, shape)
-                unchanged[name] = var
-            tf.compat.v1.train.init_from_checkpoint(checkpoint, unchanged)
-
-    def get_params(self):
-        return self.model.get_params() + [self.reward_gain, self.reward_bias]
-
-    def score_fn(self, queries, responses):
-        tokens = tf.concat([queries, responses], axis=1)
-        return self._build(tokens)
+# class TrainedRewardModel():
+#     def __init__(self, train_dir, encoding, *, scope='reward_model', comm=MPI.COMM_WORLD):
+#         self.train_dir = train_dir
+#         self.comm = comm
+#
+#         self.encoding = encoding
+#         encoder = encoding.get_encoder()
+#         if train_dir != 'test':
+#             self.hparams = trained_models.load_hparams(os.path.join(train_dir, 'hparams.json'))
+#             assert self.hparams.n_vocab == encoding.n_vocab, f'{self.hparams.n_vocab} != {encoding.n_vocab}'
+#         else:
+#             self.hparams = trained_models.test_hparams()
+#
+#         self.padding_token = encoder.padding_token
+#
+#         self.encoder = encoder
+#
+#         self.scope = scope
+#         self.model = model.Model(hparams=self.hparams, scope=f'{scope}/model', scalar_heads=['reward'])
+#
+#     def _build(self, X):
+#         results = self.model(X=X, padding_token=self.padding_token)
+#         reward = results['reward'][:, -1]
+#         with tf.compat.v1.variable_scope(f'{self.scope}/reward_norm'):
+#             self.reward_gain = tf.compat.v1.get_variable('gain', shape=(), initializer=tf.compat.v1.constant_initializer(1))
+#             self.reward_bias = tf.compat.v1.get_variable('bias', shape=(), initializer=tf.compat.v1.constant_initializer(0))
+#         reward = self.reward_gain * reward + self.reward_bias
+#         self._set_initializers()
+#         return reward
+#
+#     def ensure_built(self):
+#         if self.model.built:
+#             return
+#         with tf.compat.v1.name_scope('dummy'):
+#             self._build(X=tf.zeros([0,0], dtype=tf.int32))
+#
+#     def _set_initializers(self):
+#         """Change initializers to load a model from a tensorflow checkpoint."""
+#         if self.comm.Get_rank() > 0 or self.train_dir == 'test':
+#             return
+#
+#         assert self.model.built
+#         checkpoint_scope = 'reward_model'
+#
+#         with tf.init_scope():
+#             # Initialize!
+#             params = {v.op.name: v for v in self.get_params()}
+#             checkpoint = tf.train.latest_checkpoint(os.path.join(self.train_dir, 'checkpoints/'))
+#             available = tf.train.list_variables(checkpoint)
+#             unchanged = {}
+#
+#             for name, shape in available:
+#                 if not name.startswith(checkpoint_scope + '/'):
+#                     # print('skipping', name)
+#                     continue
+#                 if name.endswith('adam') or name.endswith('adam_1'):
+#                     # print('skipping', name)
+#                     continue
+#                 print('setting', name)
+#                 var = params[self.scope + name[len(checkpoint_scope):]]
+#                 assert var.shape == shape, 'Shape mismatch: %s.shape = %s != %s' % (var.op.name, var.shape, shape)
+#                 unchanged[name] = var
+#             tf.compat.v1.train.init_from_checkpoint(checkpoint, unchanged)
+#
+#     def get_params(self):
+#         return self.model.get_params() + [self.reward_gain, self.reward_bias]
+#
+#     def score_fn(self, queries, responses):
+#         tokens = tf.concat([queries, responses], axis=1)
+#         return self._build(tokens)
